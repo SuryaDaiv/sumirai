533f8566a64559887d83e88c05db5d513ad92ab3
SuryaDaiv <suryaveera96@gmail.com>
2026-01-31 00:55:40 -0600

    Fix font loading error to resolve black screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { Canvas } from '@react-three/fiber'
-import { ScrollControls, Scroll } from '@react-three/drei'
+import { ScrollControls, Loader } from '@react-three/drei'
 import { Suspense } from 'react'
 import Scene from './components/Scene'
 
 function App() {
   return (
     <>
-      <Canvas shadows camera={{ position: [0, 0, 5], fov: 30 }}>
+      <Canvas
+        shadows
+        camera={{ position: [0, 0, 5], fov: 30 }}
+        style={{ width: '100vw', height: '100vh', background: '#050505' }}
+      >
         <color attach="background" args={['#050505']} />
         <Suspense fallback={null}>
           <ScrollControls pages={5} horizontal damping={0.1}>
-             <Scene />
+            <Scene />
           </ScrollControls>
         </Suspense>
       </Canvas>
+      <Loader />
     </>
   )
 }
